77ebc60dcab16fc8b3dd8e2240ffd9eaac38d18d
Matthew Meade <mafumeade@gmail.com>
2018-09-15 09:42:15 -0230

    Added tabs

---
+++ server/public/scripts/tabs.js
@@ -0,0 +1,8 @@
+$("nav li").on("click", e => {
+  $(".active").forEach(element => {
+    element.classList.remove("active");
+  });
+
+  e.target.classList.add("active");
+  $("#" + e.target.innerText.toLowerCase() + "Tab")[0].classList.add("active");
+});
