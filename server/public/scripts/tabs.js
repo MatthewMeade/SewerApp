25d67a80473a6d3479857d6e658da8b7fd54bf07
Matthew Meade <mafumeade@gmail.com>
2018-09-26 20:37:48 -0230

    Add tab linking

--- server/public/scripts/tabs.js
+++ server/public/scripts/tabs.js
@@ -1,13 +1,22 @@
-$("nav li:not(#logout)").on("click", e => {
+const changeTab = name => {
   $(".active").forEach(element => {
     element.classList.remove("active");
   });
 
-  e.target.classList.add("active");
-  $("#" + e.target.innerText.toLowerCase() + "Tab")[0].classList.add("active");
+  $(`nav li.${name}`)[0].classList.add("active");
+  $(`#${name}Tab`)[0].classList.add("active");
+};
+
+$("nav li:not(#logout)").on("click", e => {
+  changeTab(e.target.classList[0]);
 });
 
 $("#logout").on("click", () => {
   axios.delete("/users/me/token");
   window.location.href = "/";
 });
+
+const url = window.location.href.split("#");
+if (url.length > 1) {
+  changeTab(url.pop());
+}
