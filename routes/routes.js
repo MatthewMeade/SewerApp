e348e74f773b5d91828521824fe5c508d57c7926
Matthew Meade <mafumeade@gmail.com>
2018-11-01 21:57:16 -0230

    Add tab titles and favicon

--- routes/routes.js
+++ routes/routes.js
@@ -6,12 +6,20 @@ const router = express.Router();
 // const reviewController = require('../controllers/reviewController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/', (req, res) => res.render('index.pug'));
-router.get('/home', (req, res) => res.render('index.pug'));
+router.get('/', (req, res) => res.render('index.pug', { title: 'Home' }));
+router.get('/home', (req, res) => res.render('index.pug', { title: 'Home' }));
 
-router.get('/systems', (req, res) => res.render('systems.pug'));
-router.get('/clients', (req, res) => res.render('clients.pug'));
-router.get('/invoices', (req, res) => res.render('invoices.pug'));
-router.get('/settings', (req, res) => res.render('settings.pug'));
+router.get('/systems', (req, res) =>
+  res.render('systems.pug', { title: 'Systems' }),
+);
+router.get('/clients', (req, res) =>
+  res.render('clients.pug', { title: 'Clients' }),
+);
+router.get('/invoices', (req, res) =>
+  res.render('invoices.pug', { title: 'Invoices' }),
+);
+router.get('/settings', (req, res) =>
+  res.render('settings.pug', { title: 'Settings' }),
+);
 
 module.exports = router;
