b62da07a09ba22488af67c1b92adc6839aa14223
Matthew Meade <mafumeade@gmail.com>
2019-02-05 20:41:11 -0330

    Change toggle setup to change modal form action based on button pressed

--- public/javascripts/modules/toggleModal.js
+++ public/javascripts/modules/toggleModal.js
@@ -2,11 +2,15 @@ import { $, $$ } from "./Bling";
 
 function setupToggle(modal, openBtn) {
   if (!modal || !openBtn) return;
-  openBtn.on("click", () => modal.classList.add("is-active"));
 
   modal.querySelector(".modal-background").onclick = () => modal.classList.remove("is-active");
   modal.querySelector(".cancel").onclick = () => modal.classList.remove("is-active");
   modal.querySelector(".close").onclick = () => modal.classList.remove("is-active");
+
+  openBtn.on("click", () => {
+    modal.classList.add("is-active");
+    modal.querySelector("form").action = openBtn.dataset.url;
+  });
 }
 
 export default setupToggle;
